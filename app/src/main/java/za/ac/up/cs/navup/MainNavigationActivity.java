5fe5d5147d32baa640566c028191b30e025ee2a8
Merissa Joubert <robogirl1996@gmail.com>
2017-04-04 14:49:26 +0200

    Added map functionality to main activity page

--- app/src/main/java/za/ac/up/cs/navup/MainNavigationActivity.java
+++ app/src/main/java/za/ac/up/cs/navup/MainNavigationActivity.java
@@ -1,9 +1,12 @@
+
 package za.ac.up.cs.navup;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,11 +18,32 @@ import android.view.MenuItem;
 
 public class MainNavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+  /**
+      * Builds the map when the Google Play services client is successfully connected.
+      */
+  private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mLocationPermissionGranted;
+    
+    public void onConnected(Bundle connectionHint) {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_navigation);
+         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+         mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+        .addOnConnectionFailedListener(this)
+        .addApi(LocationServices.API)
+        .build();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -41,7 +65,31 @@ public class MainNavigationActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+   /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        //googleMap.setBuildingsEnabled(false);
 
+            LatLng sydney = new LatLng(-25.754549, 28.231448);
+            googleMap.addMarker(new MarkerOptions().position(sydney)
+                    .title("University of Pretoria"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            //googleMap.setMaxZoomPreference(20);
+        googleMap.animateCamera( CameraUpdateFactory.zoomTo( 20.0f ) );
+
+
+
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
